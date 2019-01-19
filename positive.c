ed7d7e688bf626170f17ebea2db0f407b9bf120c
pavinraj <46838694+pavinraj@users.noreply.github.com>
2019-01-19 14:50:36 +0530

    Update positive.c

--- positive.c
+++ positive.c
@@ -1,1 +1,28 @@
-gfuiu
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int n,t;
+  clrscr();
+  printf("enter test case");
+  scanf("%d",&t);
+  while(t!=0)
+  {
+  printf("enter value");
+  scanf("%d",&n);
+  if(n>0)
+  {
+    printf("positive");
+  }
+  elseif(n<0)
+  {
+    printf("negative");
+  }
+  else
+  {
+    printf("zero");
+  }
+  t--;
+  }
+  getch();
+}
