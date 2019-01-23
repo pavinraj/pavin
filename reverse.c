99aac791d4dd0fa8b48f412d583182eb94ede5bf
pavinraj <46838694+pavinraj@users.noreply.github.com>
2019-01-23 15:45:28 +0530

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -5,7 +5,15 @@ void main()
 char a[100],i,j,b[100];
 printf("enter string");
 scanf("%s",&a);
-b=strrev(a);
-printf("%s",b);
+  for(i=0;a[i]!='\0';i++)
+      {
+        c++;
+      }
+for(i=c-1;a[i]=0;i++)
+{
+  b[j]=a[i];
+  j++;
+}
+  printf("%s",b);
 getch();
 }
