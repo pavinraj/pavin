ce74f6450ccf176a68d7162a95bd91fc1a46aa2c
pavinraj <46838694+pavinraj@users.noreply.github.com>
2019-01-28 20:21:28 +0530

    Update Power.c

--- Power.c
+++ Power.c
@@ -2,11 +2,12 @@
 int main()
 {
 int n1,n2,i,j=1;
+  printf("enter values");
 scanf("%d %d",&n1,&n2);
 for(i=0;i<n2;i++)
 {
 j=j×n1;
 }
-printf("%d",j);
+printf("power of two number is %d",j);
 return 0;
 }
