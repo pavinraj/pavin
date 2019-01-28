5c0e5dd025cd8f02e2c7b4c6a20276e382341684
pavinraj <46838694+pavinraj@users.noreply.github.com>
2019-01-28 21:52:23 +0530

    Update and rename Armstrong.c to Armstrongrange.c

--- Armstrongrange.c
+++ Armstrongrange.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
 int main()
 {
-int n,i,j,r,s=0,p=1,l=0,t1,t2,t3;
-scanf("%d",&n);
-t1=n;
-t2=n;
-t3=n;
+int n1,n2,i,j,r,s=0,p=1,l=0,t1,t2,t3;
+scanf("%d%d",&n1,&n2);
+  for(j=n1;j<n2;j++)
+    {
+t1=n1;
+t2=n1;
+t3=n1;
+    l=0;
 while(n>0)
 {
 n=n/10;
 l++;
 }
+    s=0;
 while(t1>0)
 {
 r=t1%10;
@@ -24,12 +28,9 @@ t1=t1/10;
 }
 if(s==t2)
 {
-printf("yes");
+printf("%d",t2);
 }
 
-else
-{
-printf("no");
-}
+
 return 0;
 }
