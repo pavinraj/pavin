a0f29f741ca827df657bcce1e73555b2286ee860
pavinraj <46838694+pavinraj@users.noreply.github.com>
2019-02-10 06:56:13 +0530

    Create B47.c

---
+++ B47.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+ int main()
+ {
+ int a[100],i,j,n,l=0,t=0;
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+ {
+ scanf("%d",&a[i]);
+ }
+ for(i=0;i<n;i++)
+ {
+ if(a[i]>l)
+ {
+ l=a[i];
+ }
+ }
+ t=a[0];
+ for(i=0;i<n;i++)
+ {
+ if(a[i]<t)
+ {
+ t=a[i];
+ }
+ }
+ printf("%d %d",l,t);
+ return 0;
+ }
+ 
