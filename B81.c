327aa440f9067d06af26ea88869040f2775abbba
pavinraj <46838694+pavinraj@users.noreply.github.com>
2019-02-11 19:31:56 +0530

    Create B81.c

---
+++ B81.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int n1,n2,i;
+	for(i=1;i<=3;i++)
+	{
+	scanf("%d %d\n",&n1,&n2);
+	if(n1>n2)
+	{
+		printf("\n%d",n1-n2);
+	}
+	else
+	{
+		printf("\n%d",n2-n1);
+	}
+	}
+	return 0;
+}
