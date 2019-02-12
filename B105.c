8fa5f04b51d34fa73d6048a9df46333616e68daf
pavinraj <46838694+pavinraj@users.noreply.github.com>
2019-02-12 22:03:21 +0530

    Create B105.c

---
+++ B105.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{ int j;
+    char n[30],k[30],val[30];
+    scanf("%s",n);
+    scanf("%s",k);
+    
+    printf("\n%s",strcat(n,k));
+    return 0;
+}
