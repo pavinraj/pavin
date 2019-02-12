792ed18ff92dc1f3b48230022f4ec00b0598d317
pavinraj <46838694+pavinraj@users.noreply.github.com>
2019-02-12 22:12:13 +0530

    Rename B106.c to B107.c

---
+++ B107.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+
+{ int j;
+    int a,b,c,val=0;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    val=(a*b)/c;
+    printf("\n%d",val);
+    
+    
+    return 0;
+
+    
+}
