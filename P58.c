784f0b8fd280467225d9e2d2f1f082fc6e8e6795
pavinraj <46838694+pavinraj@users.noreply.github.com>
2019-02-28 06:27:28 +0530

    Create P58.c

---
+++ P58.c
@@ -0,0 +1,37 @@
+
+  
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char a[100],b[100];
+int i,j,n,k=0,t[250],m,q=0,e=0;
+clrscr();
+printf("\nEnter the sentence : ");
+gets(a);
+printf("\nEnter the word : ");
+gets(b);
+m=strlen(b);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]==' '||a[i+1]=='\0')
+{
+for(j=k;j<=i;j++)
+{
+if(a[j]==b[q])
+{
+q++;
+}
+}
+if(q==m)
+{
+e=e+1;
+}
+q=0;
+k=i+1;
+}
+}
+printf("\nThere are %d times (%s) is occuring in the sentence.",e,b);
+getch();
+}
