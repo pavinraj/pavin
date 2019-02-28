02c969d92b8b3ba27633d00bf0b97be3268fe1c9
pavinraj <46838694+pavinraj@users.noreply.github.com>
2019-02-28 06:22:03 +0530

    Create P59.c

---
+++ P59.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+int main()
+{
+    int a[100000],N,i,c;
+    scanf("%d",&N);
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<N;i++)
+    {
+        if(a[i]==0)
+        {
+            c=i;
+        }
+    }
+    for(i=0;i<c;i++)
+    {
+        if(a[i]!=0)
+        {
+        printf("%d ",a[i]);
+        }
+    }
+
+}
