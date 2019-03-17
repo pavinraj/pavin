291ed08f3f6f83f40b5d3dbeb40740f88d2c34f3
pavinraj <46838694+pavinraj@users.noreply.github.com>
2019-03-17 20:31:49 +0530

    Create P119.c

---
+++ P119.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<string.h>
+int main() 
+{
+	
+  char a[30];
+	scanf("%s",a);
+	int i,l,sum=0,b=1;
+	l=strlen(a);
+	for(i=l-1;i>=0;i--)
+	{
+		if(a[i]>='A' && a[i]<='F')
+		{
+			a[i]=(a[i]-65)+10;
+		}
+		else if(a[i]>='0' && a[i]<='9')
+		{
+			a[i]=a[i]-48;
+		}
+		sum=sum+(a[i]*b);
+		b=b*16;
+	}
+	printf("%d",sum);
+	return 0;
+}
