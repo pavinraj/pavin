f5ca1a803eeeb359fd1b3d2054dc3a16047171cc
pavinraj <46838694+pavinraj@users.noreply.github.com>
2019-03-25 05:38:22 +0530

    Create P178.c

---
+++ P178.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+	char str[30];
+	scanf("%[^\n]s",str);
+	int i,j;
+	for(i=0;str[i]!='\0';i++)
+	{
+		for(j=i+1;str[j]!='\0';j++)
+		{
+		 if(str[i]==str[j])
+
+{
+		 	str[j]=toupper(str[j]);
+		 }
+		}
+		str[i]=toupper(str[i]);
+	}
+	printf("%s",str);
+}
